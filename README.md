b5e38f0bb3b52ff61c5c50eb46b286f2e3247b53
Koki Yamaguchi <kiddiidid@gmail.com>
2023-05-10 17:54:14 -0700

    added files

---
+++ README.md
@@ -0,0 +1,15 @@
+# cs2520_e-commerce
+
+Steps of Cloning this project:
+
+
+STEP1: Clone this repository
+
+STEP2: Create Virtual Env in your own machine
+
+STEP3: Activate the Virtual Env
+
+STEP4: Install the requirements.txt dependencies: **pip install -r requirements.txt**
+
+
+**when you pushing make sure you don't push the virtual env files**
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+# when opening the site, users should see a search bar, login button, a shopping cart. Some other potential buttons and features could be recommended
+# selections(random) and some potential filters (sports, clothes, toys)
+# The user can 
+@app.route('/')
+def index():
+    # render template is a function from flask that is used to render HTML templates
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    if query:
+        return redirect(url_for('search_results', query=query))
+    return render_template('search.html')
+
+@app.route('/search-results')
+def search_results():
+    query = request.args.get('query')
+    # Perform search logic here
+    results = []
+    return render_template('search_results.html', query=query, results=results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
